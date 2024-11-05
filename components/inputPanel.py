4238f36071d2d2c7fbf149fa5e00b65e82824b49
topaccina <54760952+topaccina@users.noreply.github.com>
2024-11-05 22:36:59 +0100

    added style and info button

--- components/inputPanel.py
+++ components/inputPanel.py
@@ -9,20 +9,54 @@ inputTitle = html.H4(
     className=" mt-2  p-3 ",
 )
 
-inputArea = html.Div(
+popover_info = 'Start to chat with your assistant. Introduce yourself and ask about what you can learn in this app. Once your journey is done, by clicking "end" you can download your chat history '
+
+inputArea = dbc.Container(
     [
-        dbc.Textarea(
-            className="mb-3 mt-1 p-3 ",
-            placeholder="Chat with your assistant, ask help here.",
-            size="sm",
-            id="id-inputArea",
-        ),
-        dbc.ButtonGroup(
+        dbc.Row(
             [
-                dbc.Button(["Submit"], id="id-submitButton"),
-                dbc.Button(["End"], id="id-endButton"),
-            ],
-            className="mb-2",
+                dbc.Col(
+                    [
+                        dbc.Textarea(
+                            className="mb-3 mt-1 p-3 ",
+                            placeholder="Chat with your assistant, ask help here.",
+                            size="sm",
+                            id="id-inputArea",
+                        )
+                    ]
+                ),
+            ]
+        ),
+        dbc.Row(
+            [
+                dbc.Col(
+                    [
+                        dbc.ButtonGroup(
+                            [
+                                dbc.Button(["Submit"], id="id-submitButton"),
+                                dbc.Button(["End"], id="id-endButton"),
+                            ],
+                            className="mb-2",
+                        ),
+                    ],
+                    width=3,
+                ),
+                dbc.Col(
+                    [
+                        dbc.Button(
+                            ["Info"],
+                            id="id-InfoButton",
+                            className="mb-2",
+                        ),
+                        dbc.Popover(
+                            popover_info,
+                            target="id-InfoButton",
+                            body=True,
+                            trigger="hover",
+                        ),
+                    ]
+                ),
+            ]
         ),
     ],
 )
